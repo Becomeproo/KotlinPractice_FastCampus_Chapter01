4ab59ac60702b1973fd5d13c580dea5fbe60839a
KKW <yesrjsdn@gmail.com>
2022-03-19 11:30:58 +0900

    KotlinPractice_FastCampus_Chapter01

--- app/src/main/java/com/example/practicekotlin1/MainActivity.kt
+++ app/src/main/java/com/example/practicekotlin1/MainActivity.kt
@@ -7,7 +7,13 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() { // AppCompatActivity 상속
+
+    /*
+    '?'와 '!'의 차이점
+    '?'는 Null 값이 될 수 있다는 것을 의미하고
+    '!'는 Null 값이 될 수 없다는 것을 의미함. (값을 무조건 반환한다.)
+     */
 
     private val editTextHeight: EditText by lazy {
         findViewById(R.id.editTextHeight)
@@ -29,16 +35,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initCheckButton() {
-        buttonCheck.setOnClickListener {
+        buttonCheck.setOnClickListener {  //Kotlin의 경우 인터페이스를 람다 형식으로 치환하여 바로 사용할 수 있다.
             if (editTextHeight.text.isEmpty() || editTextWeight.text.isEmpty()) {
                 Toast.makeText(this, "입력되지 않은 부분이 있습니다.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
-            val height = editTextHeight.text.toString().toInt()
-            val weight = editTextWeight.text.toString().toInt()
+            val height: Int = editTextHeight.text.toString().toInt()
+            val weight: Int = editTextWeight.text.toString().toInt()
 
-            val intent = Intent(this, ResultActivity:: class.java)
+            val intent = Intent(this, ResultActivity::class.java)
             intent.putExtra("height", height)
             intent.putExtra("weight", weight)
             startActivity(intent)
